b6ec2c7e66bf6cf9543cf748ddffea34a342d60e
JohnAlston11 <31191129+JohnAlston11@users.noreply.github.com>
2018-05-14 20:12:23 -0400

    been a while

--- web/client/src/components/home/home.js
+++ web/client/src/components/home/home.js
@@ -29,9 +29,9 @@ export default class Home extends Component{
                 <a href="/courts/bronx" > <div style={{backgroundImage:`url('${bx}'`}} id='bx' className='icons' >BRONX </div></a>
                 <a href="/courts" ><div style={{backgroundImage:`url('http://triborodesign.com/public/user-content/files/2014/02/16/nikenyc_01-1142.jpg')`}} id='all' className='icons' > ALL BOROUGHS </div></a>
                 <a href="/courts/brooklyn" ><div style={{backgroundImage:`url('${bk}')`}} id='bk' className='icons' > BROOKLYN </div></a>
-                <div style={{backgroundImage:`url('${m}')`}} id='mh' className='icons' ><Link to="/courts/manhattan" > MANHATTAN </Link></div>
-                <div style={{backgroundImage:`url('${q}')`}} id='q' className='icons' ><Link to="/courts/queens" > QUEENS </Link></div>
-                <div style={{backgroundImage:`url('${nyc}')`}} id='other' className='icons' ><Link to="/courts/other" > OTHER </Link></div>
+                <a href="/courts/manhattan" ><div style={{backgroundImage:`url('${m}')`}} id='mh' className='icons' > MANHATTAN </div></a>
+                <a href="/courts/queens" ><div style={{backgroundImage:`url('${q}')`}} id='q' className='icons' > QUEENS </div></a>
+                <a href="/courts/other" ><div style={{backgroundImage:`url('${nyc}')`}} id='other' className='icons' > OTHER </div></a>
             </div>
         )
     }
