658e460099bdbf49643c702f2fd26c30d9cca66c
JohnAlston11 <31191129+JohnAlston11@users.noreply.github.com>
2018-05-30 15:00:34 -0400

    can chat

---
+++ web/db.js
@@ -0,0 +1,8 @@
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database : process.env.DB_NAME,
+  socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
+})
