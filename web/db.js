9c512f90a65882f923e5a9a9ec644022af8551c7
JohnAlston11 <31191129+JohnAlston11@users.noreply.github.com>
2018-12-03 08:54:32 -0500

    working on dependencies

--- web/db.js
+++ web/db.js
@@ -4,10 +4,10 @@ var connection = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database : process.env.DB_NAME,
-  socketPath: '/var/run/mysqld/mysqld.sock'
+  socketPath: '/Applications/MAMP/tmp/mysql/mysql.sock'
 })
 
-connection.connect()
+connection.connect();
 // connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
 //   if (error) throw error;
 //   console.log('The solution is: ', results[0].solution);
